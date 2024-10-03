c70ce86939612f9df331aab1d8dbe2bcce2e03ef
HE-MAN-22603 <himanshu22603@gamil.com>
2024-10-04 01:43:14 +0530

    2D_Array

--- 4_2D_matrix_90degrees_rotation.cpp
+++ 4_2D_matrix_90degrees_rotation.cpp
@@ -42,7 +42,7 @@ int main()
     
     for ( i = 0; i < n; i++)
     {
-        for ( j = n-1; j >=0; --j)
+        for ( j = 0; j < n; j++)
         {
             temp=arr[i][j];
             arr[i][j]= arr[i][j];
